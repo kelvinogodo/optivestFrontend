f42f8794b22de26510bd7b689ed6f29e62aad00d
trilvid <trilztech34@gmail.com>
2025-05-01 00:06:32 +0100

    first commit

--- src/components/tradingbot/TradingBot.jsx
+++ src/components/tradingbot/TradingBot.jsx
@@ -111,7 +111,7 @@ const TradingBot = ({ route }) => {
                             <path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
                           </svg>
                         </span>
-                        <span> <strong> Fast</strong></span>
+                        <span> <strong> durable</strong></span>
                       </li>
                       <li>
                         <span class="icon">

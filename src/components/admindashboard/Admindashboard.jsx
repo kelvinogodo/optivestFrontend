a9e1c562a14432431cf4f7cdc5d4035d8a026a32
trilvid <trilztech34@gmail.com>
2025-04-30 15:11:15 +0100

    first commit

--- src/components/admindashboard/Admindashboard.jsx
+++ src/components/admindashboard/Admindashboard.jsx
@@ -307,7 +307,7 @@ const Admindashboard = ({ route }) => {
                     login()
                     }}>
           <div class="logo_container" onClick={()=>navigate('/')}>
-            <img src="/stockedgelogo5.png" alt="" />
+            <img src="/boardbanklogo6.png" alt="" />
           </div>
           <div class="title_container">
             <p class="titles">welcome admin</p>
